0d2bbccef5f448e031e775e04e4ab34d28537b4e
fhavy <favvyghabs@gmail.com>
2023-01-31 18:36:37 +0100

    initial commit

---
+++ opensourcepos-master/application/language/ar-LB/login_lang.php
@@ -0,0 +1,12 @@
+<?php
+$lang["login_gcaptcha"] = "أنا لست روبوت.";
+$lang["login_go"] = "البدء";
+$lang["login_invalid_gcaptcha"] = "يرجى التحقق من أنك لست روبوتًا.";
+$lang["login_invalid_installation"] = "يوجد مشكلة بالتنصيب, الرجاء التحقق من ملف php.ini.";
+$lang["login_invalid_username_and_password"] = "اسم المستخدم/كلمة المرور غير صحيحة.";
+$lang["login_login"] = "دخول";
+$lang["login_logout"] = "تسجيل خروج";
+$lang["login_migration_needed"] = "سيبدأ ترحيل قاعدة البيانات إلى٪ 1 بعد تسجيل الدخول.";
+$lang["login_password"] = "كلمة السر";
+$lang["login_username"] = "اسم المستخدم";
+$lang["login_welcome"] = "مرحباً بك في٪ 1!";
